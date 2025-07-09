4ca0e596bb18c7344ec8b54f0dfbc22eca880a33
Muhammad-Faizmf <faizanmushtaq3@gmail.com>
2025-07-09 17:22:21 +0500

    add cloudinary config and multer package

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,28 @@
+import { v2 as cloudinary } from "cloudinary";
+const fs = require("fs");
+
+// Configuration
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const uploadCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) return null;
+    const uploadResult = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+    console.log("File uploaded on cloudinary: ", uploadResult.url);
+    return uploadResult;
+  } catch (error) {
+    fs.unlinkSync(localFilePath); // remove the saved file locally
+    console.log(error);
+    return null;
+  }
+};
+
+module.exports = {
+  uploadCloudinary,
+};
